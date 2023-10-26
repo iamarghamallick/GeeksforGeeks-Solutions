46d236627bbf34de2490becf9ef58022b62eeb7c
Argha Mallick <86346861+iamarghamallick@users.noreply.github.com>
2023-10-26 20:29:15 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Minimum Operations/minimum-operations.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+  public:
+    int minOperation(int n)
+    {
+        //code here.
+        int count = 0;
+        while(n != 0) {
+            if(n%2==0) {
+                n /= 2;
+                count++;
+            } else {
+                n--;
+                count++;
+                if(n>0) {
+                    n /= 2;
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	    {
+	        int n;
+	        cin>>n;
+	        Solution ob;
+	        cout<<ob.minOperation(n)<<endl;
+	    }
+}
+// } Driver Code Ends
