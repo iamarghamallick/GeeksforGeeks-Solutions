1c2a02d854e6a703c9f21c025dd71e499452e91c
Argha Mallick <86346861+iamarghamallick@users.noreply.github.com>
2023-11-29 17:35:35 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Euler circuit and Path/euler-circuit-and-path.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+public:
+	int isEulerCircuit(int V, vector<int>adj[]){
+	    // Code here
+	    int count = 0;
+	    
+	    for(int i=0; i<V; i++) {
+	        if(adj[i].size() % 2 == 0)  count++;
+	    }
+	    
+	    if(count == V)  return 2;
+	    if(count == V-2)    return 1;
+	    return 0;
+	}
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int V, E;
+		cin >> V >> E;
+		vector<int>adj[V];
+		for(int i = 0; i < E; i++){
+			int u, v;
+			cin >> u >> v;
+			adj[u].push_back(v);
+			adj[v].push_back(u);
+		}
+		Solution obj;
+		int ans = obj.isEulerCircuit(V, adj);
+		cout << ans <<"\n";	}
+	return 0;
+}
+// } Driver Code Ends
