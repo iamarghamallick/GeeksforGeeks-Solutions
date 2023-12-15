9d4bac75a54390bc9dd466c406e2e6bcfca28e27
Argha Mallick <86346861+iamarghamallick@users.noreply.github.com>
2023-12-15 17:31:35 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Reach the Nth point/reach-the-nth-point.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+	public:
+		int nthPoint(int n){
+		    // Code here
+		    int a = 1;
+		    int b = 2;
+		    int mod = 1000000007;
+		    
+		    if(n < 3)   return n;
+		    
+		    for(int i=3; i<=n; i++) {
+		        int temp = a;
+		        a = b % mod;
+		        b = (temp + a) % mod;
+		    }
+		    
+		    return b;
+		}
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		Solution ob;
+		int ans  = ob.nthPoint(n);
+		cout << ans <<"\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
