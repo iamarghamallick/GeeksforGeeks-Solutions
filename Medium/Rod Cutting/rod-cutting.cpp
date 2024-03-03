e1e48667957997cd5d73b0a8b9d12942f21824b0
Argha Mallick <86346861+iamarghamallick@users.noreply.github.com>
2024-03-03 20:22:07 +0530

    Updated Solution - GfG to GitHub

--- Medium/Rod Cutting/rod-cutting.cpp
+++ Medium/Rod Cutting/rod-cutting.cpp
@@ -12,7 +12,12 @@ class Solution{
   public:
     int cutRod(int price[], int n) {
         //code here
+        // this is the same problem as 0 / 1 knapsack with allowed repeation
+        // where wt[] === length[] = {1,2,3,...,n-1}
+        // and val[] === price[]
+        // with W === N
         
+        // initializing dp array
         int dp[n+1][n+1];
         
         for(int i=0; i<=n; i++) {
@@ -22,9 +27,12 @@ class Solution{
         
         for(int i=1; i<=n; i++) {
             for(int j=1; j<=n; j++) {
+                // here length[i] = i
                 if(i <= j) {
+                    // we have two choise 
                     dp[i][j] = max(price[i-1] + dp[i][j - i], dp[i-1][j]);
                 } else {
+                    // no choise -> cannot include
                     dp[i][j] = dp[i-1][j];
                 }
             }
