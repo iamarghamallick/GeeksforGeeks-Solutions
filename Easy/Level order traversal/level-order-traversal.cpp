700f48bb15b47026ee6d87146ded280b78891f81
Argha Mallick <86346861+iamarghamallick@users.noreply.github.com>
2024-03-18 16:59:48 +0530

    Updated Solution - GfG to GitHub

--- Easy/Level order traversal/level-order-traversal.cpp
+++ Easy/Level order traversal/level-order-traversal.cpp
@@ -38,34 +38,25 @@ struct Node
  */
 
 
-class Solution
-{
+class Solution {
     public:
     //Function to return the level order traversal of a tree.
-    vector<int> levelOrder(Node* root)
-    {
+    vector<int> levelOrder(Node* root) {
         //Your code here
-        vector<int> ans;
         queue<Node*> q;
-      
+        vector<int> v;
         q.push(root);
-      
+        
         while(!q.empty()) {
-            int n = q.size();
-            for(int i=0; i<n; i++) {
-                Node* currNode = q.front();
-                q.pop();
-              
-                ans.push_back(currNode->data);
-              
-                if(currNode->left)
-                    q.push(currNode->left);
-                if(currNode->right)
-                    q.push(currNode->right);
-            }
+            Node* temp = q.front();
+            v.push_back(temp->data);
+            q.pop();
+            
+            if(temp->left)  q.push(temp->left);
+            if(temp->right) q.push(temp->right);
         }
         
-        return ans;
+        return v;
     }
 };
 
