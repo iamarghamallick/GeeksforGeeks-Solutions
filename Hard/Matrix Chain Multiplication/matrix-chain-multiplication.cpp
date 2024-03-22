f9565a384de2027c86bf5c21184f2c1ca4cce54f
Argha Mallick <86346861+iamarghamallick@users.noreply.github.com>
2024-03-22 17:50:27 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Matrix Chain Multiplication/matrix-chain-multiplication.cpp
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    // Approch 1: Only recursive, Res: Time Limit Exceeded
+    // int solve(int arr[], int i, int j) {
+    //     // base case
+    //     if(i >= j)  return 0;
+        
+    //     // next logic
+    //     int ans = INT_MAX;
+    //     for(int k=i; k<j; k++) {
+    //         int tempAns = solve(arr, i, k) + solve(arr, k+1, j) + (arr[i-1] * arr[k] * arr[j]);
+    //         ans = min(ans, tempAns);
+    //     }
+        
+    //     return ans;
+    // }
+    
+    // Approch 2: Recursion with memoization
+    int dp[101][101];
+    
+    int solve(int arr[], int i, int j) {
+        // base case
+        if(i >= j)  return 0;
+        
+        // next logic
+        int ans = INT_MAX;
+        
+        // if the current sub-problem is already solved before
+        if(dp[i][j] != -1)  return dp[i][j];
+        
+        // otherwise
+        for(int k=i; k<j; k++) {
+            int tempAns = solve(arr, i, k) + solve(arr, k+1, j) + (arr[i-1] * arr[k] * arr[j]);
+            ans = min(ans, tempAns);
+        }
+        
+        return dp[i][j] = ans;
+    }
+    
+    int matrixMultiplication(int N, int arr[]) {
+        memset(dp, -1, sizeof(dp));
+        
+        return solve(arr, 1, N-1);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        int arr[N];
+        for(int i = 0;i < N;i++)
+            cin>>arr[i];
+        
+        Solution ob;
+        cout<<ob.matrixMultiplication(N, arr)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
