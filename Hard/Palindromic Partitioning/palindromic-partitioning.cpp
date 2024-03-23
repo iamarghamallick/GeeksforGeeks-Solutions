93b4776c14b63c7d0a7e87d9582d4fbbe84febd9
Argha Mallick <86346861+iamarghamallick@users.noreply.github.com>
2024-03-23 21:58:37 +0530

    Updated Solution - GfG to GitHub

--- Hard/Palindromic Partitioning/palindromic-partitioning.cpp
+++ Hard/Palindromic Partitioning/palindromic-partitioning.cpp
@@ -7,54 +7,101 @@ using namespace std;
 // } Driver Code Ends
 // User function Template for C++
 
-class Solution{
-public:
-    bool generate(string& s, vector<vector<bool>>& pal) {
-        int n = s.size();
-        
-        for(int i=0; i<n; i++) {
-            pal[i][i] = true;
+class Solution {
+private:
+    bool isPalindrom(string str, int i, int j) {
+        while(i < j) {
+            if(str[i] != str[j])
+                return false;
+            i++;
+            j--;
         }
         
-        for(int len=2; len<=n; len++) {
-            for(int i=0; i<=n-len; i++) {
-                int j = i+len-1;
-                
-                if(s[i]==s[j] && (len==2 || pal[i+1][j-1])) {
-                    pal[i][j] = true;
-                }
-            }
-        }
+        return true;
     }
     
-    int palindromicPartition(string s)
-    {
-        // code here
-        if(s.empty())
-            return 0;
+public:
+    // Approch 1: Only Recursion -> Result: TLE
+    // int solve(string str, int i, int j) {
+    //     // base condition
+    //     if(i >= j)  return 0;
+    //     // if the string is already a palindrom, no partitioning needed
+    //     if(isPalindrom(str, i, j))    return 0;
         
-        int n = s.size();
+    //     // next logic
+    //     int ans = INT_MAX;
+    //     for(int k=i; k<=j-1; k++) {
+    //         int tempAns = solve(str, i, k) + solve(str, k+1, j) + 1;
+    //         ans = min(ans, tempAns);
+    //     }
         
-        vector<vector<bool>> pal(n, vector<bool>(n, false));
+    //     return ans;
+    // }
+    
+    // Approch 2: Memoized Recursion -> Result: TLE
+    // int dp[501][501];
+    // int solve(string str, int i, int j) {
+    //     // base condition
+    //     if(i >= j)  return 0;
+    //     // if the string is already a palindrom, no partitioning needed
+    //     if(isPalindrom(str, i, j))    return 0;
         
-        generate(s, pal);
+    //     // if the current sub-problem is already solved
+    //     if(dp[i][j] != -1)  return dp[i][j];
         
-        vector<int> dp(n, INT_MAX);
-        dp[0] = 0;
+    //     // next logic
+    //     int ans = INT_MAX;
+    //     for(int k=i; k<=j-1; k++) {
+    //         int tempAns = solve(str, i, k) + solve(str, k+1, j) + 1;
+    //         ans = min(ans, tempAns);
+    //     }
         
-        for(int i=0; i<n; i++) {
-            if(pal[0][i])
-                dp[i] = 0;
+    //     // store the result, then return 
+    //     return dp[i][j] = ans;
+    // }
+    
+    // Approch 3: Memoized Recursion with more optimization -> Result: Accepted
+    int dp[501][501];
+    int solve(string str, int i, int j) {
+        // base condition
+        if(i >= j)  return 0;
+        // if the string is already a palindrom, no partitioning needed
+        if(isPalindrom(str, i, j))    return 0;
+        
+        // if the current sub-problem is already solved
+        if(dp[i][j] != -1)  return dp[i][j];
+        
+        // next logic
+        int ans = INT_MAX;
+        for(int k=i; k<=j-1; k++) {
+            int leftAns, rightAns;
+            
+            if(dp[i][k] != -1)
+                leftAns = dp[i][k];
             else {
-                for(int j=i; j>=1; j--) {
-                    if(pal[j][i]) {
-                        if(dp[j-1]+1 < dp[i])
-                            dp[i] = dp[j-1]+1;
-                    }
-                }
+                leftAns = dp[i][k] = solve(str, i, k);
             }
+            
+            if(dp[k+1][j] != -1)
+                rightAns = dp[k+1][j];
+            else {
+                rightAns = dp[k+1][j] = solve(str, k+1, j);
+            }
+            
+            int tempAns = leftAns + rightAns + 1;
+            ans = min(ans, tempAns);
         }
-        return dp[n-1];
+        
+        // store the result, then return 
+        return dp[i][j] = ans;
+    }
+    
+    int palindromicPartition(string str) {
+        // initializing dp array
+        memset(dp, -1, sizeof(dp));
+        
+        // function call
+        return solve(str, 0, str.size()-1);
     }
 };
 
