b9fca62f6e8d96523d8363acd641977f17f320b4
Argha Mallick <86346861+iamarghamallick@users.noreply.github.com>
2024-05-11 17:37:45 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Juggler Sequence/juggler-sequence.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    vector<long long> jugglerSequence(long long n) {
+        if(n == 1)  return {1};
+        
+        vector<long long> ans;
+        ans.push_back(n);
+        do {
+            if(n % 2 == 0)
+                n = sqrt(n);
+            else
+                n = n * sqrt(n);
+                
+            ans.push_back(n);
+        } while(n != 1);
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long n;
+        cin >> n;
+
+        Solution ob;
+        vector<long long> ans = ob.jugglerSequence(n);
+        for (long long u : ans)
+            cout << u << " ";
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
