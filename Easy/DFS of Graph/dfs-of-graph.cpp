59c4991b55256793ff78fb3734fb45f0d5df1f3b
Argha Mallick <86346861+iamarghamallick@users.noreply.github.com>
2024-05-24 19:37:14 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/DFS of Graph/dfs-of-graph.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to return a list containing the DFS traversal of the graph.
+    vector<int> dfsOfGraph(int V, vector<int> adj[]) {
+        vector<int> ans;
+        vector<bool> visited(V, false);
+        
+        dfs(adj, 0, visited, ans);
+        
+        return ans;
+    }
+    
+  private:
+    void dfs(vector<int> adj[], int v, vector<bool> &visited, vector<int>& ans) {
+        visited[v] = true;
+        ans.push_back(v);
+        
+        for(auto nei : adj[v])
+            if(!visited[nei])
+                dfs(adj, nei, visited, ans);
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >> E;
+
+        vector<int> adj[V];
+
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        // string s1;
+        // cin>>s1;
+        Solution obj;
+        vector<int> ans = obj.dfsOfGraph(V, adj);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
