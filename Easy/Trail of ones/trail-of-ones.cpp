776805408298698a122f07185aa60777738d2292
Argha Mallick <86346861+iamarghamallick@users.noreply.github.com>
2024-06-03 18:24:29 +0530

    Updated Solution - GfG to GitHub

--- Easy/Trail of ones/trail-of-ones.cpp
+++ Easy/Trail of ones/trail-of-ones.cpp
@@ -6,30 +6,46 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    int mod=1e9+7;
+    // int mod=1e9+7;
 
-    long long power(long long a,long long b){
-        long long ans=1;
-        while(b){
-            if(b%2)ans*=a;
-            b>>=1;
-            a*=a;
-            a%=mod;
-            ans%=mod;
-        }
-        return ans;
-    }
+    // long long power(long long a,long long b){
+    //     long long ans=1;
+    //     while(b){
+    //         if(b%2)ans*=a;
+    //         b>>=1;
+    //         a*=a;
+    //         a%=mod;
+    //         ans%=mod;
+    //     }
+    //     return ans;
+    // }
     
-    long long solve(int n,vector<int> &dp){
-        if(n==2) return 1;
-        if(n==3) return 3;
-        if(dp[n]!=-1)return dp[n];
-        return dp[n]=((solve(n-1,dp) + power(2,n-2))%mod+ solve(n-2,dp))%mod;
-    }
+    // long long solve(int n,vector<int> &dp){
+    //     if(n==2) return 1;
+    //     if(n==3) return 3;
+    //     if(dp[n]!=-1)return dp[n];
+    //     return dp[n]=((solve(n-1,dp) + power(2,n-2))%mod+ solve(n-2,dp))%mod;
+    // }
+    
+    // int numberOfConsecutiveOnes(int n) {
+    //     vector<int> dp(n+1,-1);
+    //     return solve(n,dp);
+    // }
     
     int numberOfConsecutiveOnes(int n) {
-        vector<int> dp(n+1,-1);
-        return solve(n,dp);
+        const long long MOD = 1'000'000'007;
+        if (n == 1 || n == 2)
+            return 1;
+
+        long long a = 1, b = 1;
+        long long res = 1;
+        for (int i = 3; i <= n; i++) {
+            long long c = (a + b) % MOD;
+            a = b;
+            b = c;
+            res = (res * 2 + a) % MOD;
+        }
+        return (int)res;
     }
 };
 
