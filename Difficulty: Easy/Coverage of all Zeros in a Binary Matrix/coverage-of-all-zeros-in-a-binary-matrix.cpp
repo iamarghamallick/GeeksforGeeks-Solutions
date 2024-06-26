e2775ddf20e4da7e44f6707a9489256bec85b359
Argha Mallick <86346861+iamarghamallick@users.noreply.github.com>
2024-06-26 17:21:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Coverage of all Zeros in a Binary Matrix/coverage-of-all-zeros-in-a-binary-matrix.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+  public:
+    int findCoverage(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int m = mat[0].size();
+        int sum = 0;
+        
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                if(mat[i][j] == 0) {
+                    // check left direction
+                    if(j+1 < m && mat[i][j+1])  sum++;
+                    
+                    // check right direction
+                    if(j-1 >= 0 && mat[i][j-1]) sum++;
+                    
+                    // check down direction
+                    if(i+1 < n && mat[i+1][j])  sum++;
+                    
+                    // check up direction
+                    if(i-1 >= 0 && mat[i-1][j]) sum++;
+                }
+            }
+        }
+        
+        return sum;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> matrix(n, vector<int>(m, 0));
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                cin >> matrix[i][j];
+        Solution obj;
+        int ans = obj.findCoverage(matrix);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
