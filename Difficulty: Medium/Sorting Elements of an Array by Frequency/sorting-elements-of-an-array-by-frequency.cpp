0df022707bbc1069ea3a9f3be0a11827d93219c3
Argha Mallick <86346861+iamarghamallick@users.noreply.github.com>
2024-08-28 19:21:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sorting Elements of an Array by Frequency/sorting-elements-of-an-array-by-frequency.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Complete this function
+    // Function to sort the array according to frequency of elements.
+    vector<int> sortByFreq(vector<int>& arr) {
+        unordered_map<int, int> map;
+        for(int num : arr)
+            map[num]++;
+        
+        sort(arr.begin(), arr.end(), [&map](const int &a, const int &b) {
+            return map[a] == map[b] ? a < b : map[a] > map[b];
+        });
+        
+        return arr;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+
+        string input;
+        int num;
+        vector<int> arr;
+
+        getline(cin, input);
+        stringstream s2(input);
+        while (s2 >> num) {
+            arr.push_back(num);
+        }
+        Solution obj;
+        vector<int> v;
+        v = obj.sortByFreq(arr);
+        for (int i : v)
+            cout << i << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
