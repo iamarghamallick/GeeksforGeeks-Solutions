c13cdcc74c7659e3b3a3bcd978d7d0ca652bab34
Argha Mallick <86346861+iamarghamallick@users.noreply.github.com>
2024-10-04 18:50:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Deletion and Reverse in Circular Linked List/deletion-and-reverse-in-circular-linked-list.cpp
@@ -0,0 +1,120 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// Structure for the linked list node
+struct Node {
+    int data;
+    struct Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+// Function to print nodes in a given circular linked list
+void printList(struct Node *head) {
+    if (head != NULL) {
+        struct Node *temp = head;
+        do {
+            cout << temp->data << " ";
+            temp = temp->next;
+        } while (temp != head);
+    } else {
+        cout << "empty" << endl;
+    }
+    cout << endl;
+}
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to reverse a circular linked list
+    Node* reverse(Node* head) {
+        Node* prev = head, *curr = head->next;
+        do {
+            Node* temp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = temp;
+        } while(curr != head);
+        head->next = prev;
+        return prev;
+    }
+
+    // Function to delete a node from the circular linked list
+    Node* deleteNode(Node* head, int key) {
+        if (head->data == key) {
+            if (head->next == head)
+                return nullptr;
+
+            Node* last = head;
+            while (last->next != head)
+                last = last->next;
+    
+            last->next = head->next;
+            return last->next;
+        }
+    
+        Node* curr = head;
+        do {
+            if(curr->next->data == key) {
+                curr->next = curr->next->next;
+                break;
+            }
+            curr = curr->next;
+        } while(curr != head);
+        
+        return head;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+
+        // Reading input into a vector
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        int key;
+        cin >> key;
+        cin.ignore();
+
+        // Creating the circular linked list
+        struct Node *head = new Node(arr[0]);
+        struct Node *tail = head;
+        for (int i = 1; i < arr.size(); ++i) {
+            tail->next = new Node(arr[i]);
+            tail = tail->next;
+        }
+        tail->next = head; // Make the list circular
+
+        Solution ob;
+
+        // Delete the node with the given key
+        head = ob.deleteNode(head, key);
+
+        // Reverse the circular linked list
+        head = ob.reverse(head);
+
+        // Print the modified list
+        printList(head);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
