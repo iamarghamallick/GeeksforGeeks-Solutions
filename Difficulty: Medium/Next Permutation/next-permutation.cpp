989d1bf14224ce75b22b45c5ca09cce05892ca85
Argha Mallick <86346861+iamarghamallick@users.noreply.github.com>
2024-11-19 17:02:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Next Permutation/next-permutation.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    void nextPermutation(vector<int>& arr) {
+        int n = arr.size(); 
+        int pivot = -1; 
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] < arr[i + 1]) {
+                pivot = i;
+                break;
+            }
+        }
+    
+        if (pivot == -1) {
+            reverse(arr.begin(), arr.end());
+            return;
+        }
+    
+        for (int i = n - 1; i > pivot; i--) {
+            if (arr[i] > arr[pivot]) {
+                swap(arr[i], arr[pivot]);
+                break;
+            }
+        }
+
+        reverse(arr.begin() + pivot + 1, arr.end());
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        int n = arr.size();
+        ob.nextPermutation(arr);
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
