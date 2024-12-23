b284677ca404772cf30b0d3ffb141816efe678e9
Argha Mallick <86346861+iamarghamallick@users.noreply.github.com>
2024-12-23 17:57:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Search in a row-wise sorted matrix/search-in-a-rowwise-sorted-matrix.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+  public:
+    // Function to search a given number in row-column sorted matrix.
+    bool searchRowMatrix(vector<vector<int>> &mat, int x) {
+        for (vector<int> &row : mat) {
+            // Perform binary search using lower_bound in the current row
+            auto lb = lower_bound(row.begin(), row.end(), x) - row.begin();
+            // Check if the found element is equal to x
+            if (lb != row.size() && row[lb] == x) 
+                return true; // Return true if found
+        }
+        // If the target is not found in any row, return false
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int> > matrix(n);
+
+        for (int i = 0; i < n; i++) {
+            matrix[i].assign(m, 0);
+            for (int j = 0; j < m; j++) {
+                cin >> matrix[i][j];
+            }
+        }
+
+        int x;
+        cin >> x;
+        Solution obj;
+        if (obj.searchRowMatrix(matrix, x))
+            cout << "true\n";
+        else
+            cout << "false\n";
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
