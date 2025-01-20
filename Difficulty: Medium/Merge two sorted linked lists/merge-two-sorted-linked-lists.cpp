f9a7d595864bfcdeddd59ad96963eb5a1eeeb83a
Argha Mallick <86346861+iamarghamallick@users.noreply.github.com>
2025-01-20 16:54:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Merge two sorted linked lists/merge-two-sorted-linked-lists.cpp
@@ -0,0 +1,102 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printList(struct Node *head) {
+    struct Node *temp = head;
+    while (temp != NULL) {
+        cout << temp->data << ' ';
+        temp = temp->next;
+    }
+    cout << "\n~\n";
+}
+
+Node *insertSorted(Node *head, int data) {
+    Node *new_node = new Node(data);
+    if (!head || head->data >= data) {
+        new_node->next = head;
+        return new_node;
+    }
+
+    Node *current = head;
+    while (current->next && current->next->data < data) {
+        current = current->next;
+    }
+    new_node->next = current->next;
+    current->next = new_node;
+    return head;
+}
+
+
+// } Driver Code Ends
+/* Link list Node
+struct Node {
+  int data;
+  struct Node *next;
+
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+class Solution {
+  public:
+    Node* sortedMerge(Node* h1, Node* h2) {
+        Node* head = (h1->data<=h2->data)?h1:h2;
+        Node* tail=head;
+        Node* a=h1;
+        Node* b=h2;
+        (h1->data<=h2->data)?a=a->next:b=b->next;
+        while(a!=NULL && b!= NULL){
+            tail->next=(a->data<=b->data)?a:b;
+            (a->data<=b->data)?a=a->next:b=b->next;
+            tail=tail->next;
+        }
+        tail->next=(b==NULL)?a:b;
+        return head;
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver program to test above functions
+int main() {
+    int T;
+    cin >> T;
+    cin.ignore();
+    while (T--) {
+        int n1, n2, tmp;
+        Node *head1 = nullptr, *head2 = nullptr;
+        string input1, input2;
+
+        getline(cin, input1); // Read the entire line for the LL1 elements
+        stringstream ss1(input1);
+        while (ss1 >> tmp) {
+            head1 = insertSorted(head1, tmp);
+        }
+
+        getline(cin, input2); // Read the entire line for the LL2 elements
+        stringstream ss2(input2);
+        while (ss2 >> tmp) {
+            head2 = insertSorted(head2, tmp);
+        }
+
+        Solution obj;
+        Node *head = obj.sortedMerge(head1, head2);
+        printList(head);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
