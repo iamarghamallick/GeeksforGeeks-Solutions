3b736765a80771658030b7e1f705643223a927d3
Argha Mallick <86346861+iamarghamallick@users.noreply.github.com>
2025-04-14 18:36:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Alien Dictionary/alien-dictionary.cpp
+++ Difficulty: Hard/Alien Dictionary/alien-dictionary.cpp
@@ -1,98 +1,140 @@
 //{ Driver Code Starts
-// Initial Template for C++
-
 #include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
-// User function Template for C++
 
 class Solution {
   public:
-    string ans;
-    vector<bool> vis,cur;
-    unordered_set<int> g[26]; // Graph
-    
-    void topological_sort(int r) {
-        vis[r]=cur[r]=1;
-        for(int i:g[r]) 
-            if(!vis[i])
-                topological_sort(i);
-        
-        ans=char(97+r)+ans;
-        cur[r]=0;
-    }
-    
-    string findOrder(string arr[], int N, int K) {
-        ans="";
-        vis.assign(26,0); 
-        cur=vis;
-      
-        for(int i=1;i<N;i++) {
-            string a=arr[i-1],b=arr[i];
-            int l=0,n=min(a.size(),b.size());
-            while(l<n && a[l]==b[l]) 
-                l++;
-            if(l!=n)
-                g[a[l]-'a'].insert(b[l]-'a');
+    string findOrder(vector<string> &words) {
+        int n = words.size();
+        vector<vector<int>> adj(26);          // For 26 letters
+        vector<bool> present(26, false);      // Marks present letters
+        vector<int> indegree(26, 0);          // For all characters
+
+        // Mark present characters
+        for (auto &word : words) {
+            for (char c : word) {
+                present[c - 'a'] = true;
+            }
         }
-      
-        for(int i=0;i<K;i++)
-            if(!vis[i])
-                topological_sort(i);
+
+        // Build graph from adjacent words
+        for (int i = 0; i < n - 1; i++) {
+            string str1 = words[i];        
+            string str2 = words[i + 1];        
+            int len = min(str1.size(), str2.size());
+            bool check = false;
+            for (int ind = 0; ind < len; ind++) {
+                if (str1[ind] != str2[ind]) {
+                    adj[str1[ind] - 'a'].push_back(str2[ind] - 'a');
+                    indegree[str2[ind] - 'a']++;
+                    check = true;
+                    break;
+                }
+            }
+            // Invalid case: word1 is longer and is prefix of word2
+            if (!check && str1.length() > str2.length()) {
+                return "";
+            }
+        }
+
+        // Topological sort (Kahn's algorithm)
+        queue<int> q;
+        for (int i = 0; i < 26; i++) {
+            if (present[i] && indegree[i] == 0) {
+                q.push(i);
+            }
+        }
+
+        string ans = ""; 
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            ans += char(node + 'a');
+
+            for (auto it : adj[node]) {
+                indegree[it]--;
+                if (indegree[it] == 0) {
+                    q.push(it);
+                }
+            }
+        }
+
+        // Check for cycle
+        for (int i = 0; i < 26; i++) {
+            if (present[i] && indegree[i] > 0) {
+                return "";  // Cycle detected
+            }
+        }
+
         return ans;
     }
 };
 
-//{ Driver Code Starts.
-string order;
 
-bool f(string a, string b) {
-    int p1 = 0;
-    int p2 = 0;
-    for (int i = 0; i < min(a.size(), b.size()) and p1 == p2; i++) {
-        p1 = order.find(a[i]);
-        p2 = order.find(b[i]);
-        //	cout<<p1<<" "<<p2<<endl;
+//{ Driver Code Starts.
+
+bool validate(const vector<string> &original, const string &order) {
+    unordered_map<char, int> mp;
+    for (const string &word : original) {
+        for (const char &ch : word) {
+            mp[ch] = 1;
+        }
+    }
+    for (const char &ch : order) {
+        if (mp.find(ch) == mp.end())
+            return false;
+        mp.erase(ch);
+    }
+    if (!mp.empty())
+        return false;
+
+    for (int i = 0; i < order.size(); i++) {
+        mp[order[i]] = i;
     }
 
-    if (p1 == p2 and a.size() != b.size())
-        return a.size() < b.size();
-
-    return p1 < p2;
+    for (int i = 0; i < original.size() - 1; i++) {
+        const string &a = original[i];
+        const string &b = original[i + 1];
+        int k = 0, n = a.size(), m = b.size();
+        while (k < n and k < m and a[k] == b[k]) {
+            k++;
+        }
+        if (k < n and k < m and mp[a[k]] > mp[b[k]]) {
+            return false;
+        }
+        if (k != n and k == m) {
+            return false;
+        }
+    }
+    return true;
 }
 
-// Driver program to test above functions
 int main() {
-    int t;
-    cin >> t;
+    string str;
+    getline(cin, str);
+    int t = stoi(str);
     while (t--) {
-        int N, K;
-        cin >> N >> K;
-        string dict[N];
-        for (int i = 0; i < N; i++)
-            cin >> dict[i];
+        getline(cin, str);
+        stringstream ss(str);
+        string curr;
+        vector<string> words;
+        while (ss >> curr)
+            words.push_back(curr);
 
-        Solution obj;
-        string ans = obj.findOrder(dict, N, K);
-        order = "";
-        for (int i = 0; i < ans.size(); i++)
-            order += ans[i];
+        vector<string> original = words;
 
-        string temp[N];
-        std::copy(dict, dict + N, temp);
-        sort(temp, temp + N, f);
+        Solution ob;
+        string order = ob.findOrder(words);
 
-        bool f = true;
-        for (int i = 0; i < N; i++)
-            if (dict[i] != temp[i])
-                f = false;
-
-        if (f)
-            cout << 1;
-        else
-            cout << 0;
-        cout << endl;
+        if (order.empty()) {
+            cout << "\"\"" << endl;
+        } else {
+            cout << (validate(original, order) ? "true" : "false") << endl;
+        }
+        cout << "~" << endl;
     }
     return 0;
 }
