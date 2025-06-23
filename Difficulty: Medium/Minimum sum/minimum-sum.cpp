19fd331f1b0fe2c02a6915575dc71da0d257a18a
Argha Mallick <86346861+iamarghamallick@users.noreply.github.com>
2025-06-23 14:13:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum sum/minimum-sum.cpp
+++ Difficulty: Medium/Minimum sum/minimum-sum.cpp
@@ -1,67 +1,51 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-// User function template for C++
-
 class Solution {
   public:
-    string minSum(vector<int> &arr) {
-        sort(arr.begin(), arr.end());
-        string num1 = "", num2 = "";
-        for(int i=0; i<arr.size(); i++) {
-            if(i&1)
-                num1 += arr[i] + '0';
-            else
-                num2 += arr[i] + '0';
-        }
-        int i1 = num1.size()-1, i2 = num2.size() - 1, carry = 0;
+    string findSum(vector<int> &arr1, vector<int> &arr2) {
         string ans = "";
-        while(i1 >= 0 || i2 >= 0) {
-            int num11 = i1 >= 0 ? num1[i1] - '0' : 0;
-            int num21 = i2 >= 0 ? num2[i2]-'0' : 0;
-            ans += (num11 + num21 + carry) % 10 + '0';
-            carry = (num11 + num21 + carry) / 10;
-            i1--;
-            i2--;
+        int carry = 0;
+        int i = arr1.size() - 1, j = arr2.size() - 1;
+        
+        while(i >= 0 || j >= 0 || carry) {
+            int sum = carry;
+            if(i >= 0) sum += arr1[i--];
+            if(j >= 0) sum += arr2[j--];
+            ans = to_string(sum % 10) + ans;
+            carry = sum / 10;
         }
-        if(carry > 0)   ans += carry + '0';
+        
+        return ans;
+    }
+    
+    string minSum(vector<int> &arr) {
+        // int n = arr.size();
+        
+        // vector<int> freq(10, 0);
+        // for(int num : arr)  freq[num]++;
+        
+        // vector<int> num1, num2;
+        // bool flag = true;
+        // for(int i=0; i<10; i++) {
+        //     while(freq[i] > 0) {
+        //         if(flag && i > 0) num1.push_back(i);
+        //         else if(i > 0) num2.push_back(i);
+        //         freq[i]--;
+        //         flag = !flag;
+        //     }
+        // }
+        
+        // return findSum(num1, num2);
+        
+        string ans;
+        int carry = 0;
+        sort(arr.begin(), arr.end());
+        for (int i = arr.size() - 1; i >= 0; i -= 2) {
+            int sum = (arr[i] + (i - 1 >= 0 ? arr[i - 1] : 0) + carry);
+            ans.push_back('0' + (sum % 10));
+            carry = (sum / 10);
+        }
+        if (carry) ans.push_back('0' + carry);
+        while (ans.size() > 1 && ans.back() == '0') ans.pop_back();
         reverse(ans.begin(), ans.end());
-        int zeroIndex = 0;
-        while(zeroIndex < ans.length()) {
-            if(ans[zeroIndex] == '0')
-                zeroIndex++;
-            else
-                break;
-        }
-        return ans.substr(zeroIndex);
+        return ans;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> a;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            a.push_back(number);
-        }
-
-        Solution ob;
-        string ans = ob.minSum(a);
-        cout << ans << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
