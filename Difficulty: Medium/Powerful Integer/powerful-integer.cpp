67aa305342bd50195d58b95a78657ba016a5d8f5
Argha Mallick <86346861+iamarghamallick@users.noreply.github.com>
2025-07-31 15:52:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Powerful Integer/powerful-integer.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    int powerfulInteger(vector<vector<int>>& intervals, int k) {
+        priority_queue<int,vector<int>,greater<int>> start, end;
+        
+        for(auto it:intervals){
+            start.push(it[0]);
+            end.push(it[1]);
+        }
+        
+        int ele = -1;
+    
+        while(!end.empty()){
+            while(!start.empty() && start.top()<=end.top()){
+                start.pop();
+            }
+            if(end.size()-start.size()>=k){
+                ele = end.top();
+            }
+            end.pop();
+        }
+        
+        return ele;
+    }
+};
