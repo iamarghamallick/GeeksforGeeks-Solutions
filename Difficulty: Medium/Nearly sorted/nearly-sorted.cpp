28df4fc7032dd53b28bb96ea70de5841a54fcfd1
Argha Mallick <86346861+iamarghamallick@users.noreply.github.com>
2025-10-22 17:08:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Nearly sorted/nearly-sorted.cpp
+++ Difficulty: Medium/Nearly sorted/nearly-sorted.cpp
@@ -1,9 +1,3 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 // User function Template for C++
 class Solution {
   public:
@@ -23,33 +17,3 @@ class Solution {
         }
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    string ts;
-    getline(cin, ts);
-    int t = stoi(ts);
-    while (t--) {
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        string ks;
-        getline(cin, ks);
-        int k = stoi(ks);
-        Solution obj;
-        obj.nearlySorted(arr, k);
-        for (int i = 0; i < arr.size(); i++) {
-            cout << arr[i] << " ";
-        }
-        cout << endl;
-        // cout << "~" << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
