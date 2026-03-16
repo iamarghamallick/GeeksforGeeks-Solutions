642be1c4f2f6c307c8bac7d22890e8c781dff08e
Argha Mallick <86346861+iamarghamallick@users.noreply.github.com>
2026-03-16 20:13:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K Sum Paths/k-sum-paths.java
+++ Difficulty: Medium/K Sum Paths/k-sum-paths.java
@@ -1,9 +1,4 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
+/*
 class Node {
     int data;
     Node left;
@@ -15,88 +10,12 @@ class Node {
         right = null;
     }
 }
+*/
 
-class GFG {
-    static Node buildTree(String str) {
-        // Corner Case
-        if (str.length() == 0 || str.equals('N')) return null;
-        String[] s = str.split(" ");
-
-        Node root = new Node(Integer.parseInt(s[0]));
-        Queue<Node> q = new LinkedList<Node>();
-        q.add(root);
-
-        // Starting from the second element
-        int i = 1;
-        while (!q.isEmpty() && i < s.length) {
-            // Get and remove the front of the queue
-            Node currNode = q.remove();
-
-            // Get the current node's value from the string
-            String currVal = s[i];
-
-            // If the left child is not null
-            if (!currVal.equals("N")) {
-
-                // Create the left child for the current node
-                currNode.left = new Node(Integer.parseInt(currVal));
-
-                // Push it to the queue
-                q.add(currNode.left);
-            }
-
-            // For the right child
-            i++;
-            if (i >= s.length) break;
-            currVal = s[i];
-
-            // If the right child is not null
-            if (!currVal.equals("N")) {
-
-                // Create the right child for the current node
-                currNode.right = new Node(Integer.parseInt(currVal));
-
-                // Push it to the queue
-                q.add(currNode.right);
-            }
-
-            i++;
-        }
-
-        return root;
-    }
-
-    public static void main(String args[]) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine().trim());
-        while (t > 0) {
-            String s = br.readLine();
-            Node root = buildTree(s);
-            int k = Integer.parseInt(br.readLine().trim());
-            Solution ob = new Solution();
-
-            System.out.println(ob.sumK(root, k));
-            t--;
-
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
-
-
-//User function Template for Java
-
-class Solution
-{
+class Solution {
     private int count = 0;
     
-    public int sumK(Node root, int k) {
-        // code here 
+    public int countAllPaths(Node root, int k) {
         HashMap<Integer, Integer> map = new HashMap<>();
         map.put(0, 1);
         totalPaths(root, map, k, 0);
@@ -104,10 +23,14 @@ class Solution
     }
     
     public void totalPaths(Node root, HashMap<Integer, Integer> map, int k, int sum) {
-        if(root == null)    return;
+        if(root == null) {
+            return;
+        }
         
         sum += root.data;
-        if(map.containsKey(sum-k))  count += map.get(sum-k);
+        if(map.containsKey(sum-k)) {
+            count += map.get(sum-k);
+        }
         map.put(sum, map.getOrDefault(sum, 0)+1);
         totalPaths(root.left, map, k, sum);
         totalPaths(root.right, map, k, sum);
