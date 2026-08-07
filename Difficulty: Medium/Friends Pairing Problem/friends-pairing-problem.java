87aef63dc7aefdc0dab7c4a68f6c596b045a8f7b
Argha Mallick <86346861+iamarghamallick@users.noreply.github.com>
2026-08-07 17:36:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Friends Pairing Problem/friends-pairing-problem.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int countFriendsPairings(int n) {
+        if(n == 1 || n == 2) return n;
+        int pre1 = 1, pre2 = 2;
+        for(int i=3; i<=n; i++) {
+            int cur = pre2 + pre1 * (i-1);
+            pre1 = pre2;
+            pre2 = cur;
+        }
+        return pre2;
+    }
+}
